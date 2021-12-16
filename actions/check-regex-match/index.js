67a7d436bbf6310af7087bbc5a6087d15d4e8fdb
matthewgeng <matthewgeng2@gmail.com>
2021-12-15 23:57:24 -0500

    Add check regex match

---
+++ actions/check-regex-match/index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+
+const run = async () => {
+  try {
+    // Create GitHub client with the API token
+    const input = core.getInput('input', { required: true });
+    // TODO sanitization of delimiter?
+    const regex = core.getInput('regex', { required: true });
+
+    const values = JSON.parse(input);
+
+    // Debug log the payload
+    core.debug(`Inputs: ${{
+      input,
+      regex,
+    }}`);
+
+    // check input type
+    if (values.some((e) => typeof e !== 'string')) {
+      core.setFailed('Array contains non-string value');
+    }
+
+    const match = values.find((value) => regex.test(value));
+
+    // Set step output context
+    core.setOutput('match', match);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+run();
