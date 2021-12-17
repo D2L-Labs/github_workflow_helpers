4a745bf044e5b7a9bf539f46220e85bc7859ad4d
matthewgeng <matthewgeng2@gmail.com>
2021-12-17 13:56:46 -0500

    remove test workflow

--- actions/validate-if-success-or-skipped/index.js
+++ actions/validate-if-success-or-skipped/index.js
@@ -1,11 +1,8 @@
 const core = require('@actions/core');
-const { context } = require('@actions/github');
 
 const run = () => {
   try {
-    core.info(JSON.stringify(context));
-    core.info(core.getInput('jobs'));
-    core.info(JSON.stringify(core.getInput('jobs')));
+    core.info(core.getInput('needs'));
   } catch (error) {
     core.setFailed(error.message);
   }
